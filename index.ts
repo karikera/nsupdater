83ac42fb09aff117292cd635ae5f85bea06aba27
karikera <karikera3@hotmail.com>
2019-10-19 22:28:29 +0900

    address check by adapter name

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
 import https = require('https');
 import fs = require('fs');
+import os = require('os');
+import { ENOENT } from 'constants';
 
-const json = JSON.stringify(JSON.parse(fs.readFileSync('key.json', 'utf-8')));
+const key = JSON.parse(fs.readFileSync('key.json', 'utf-8'));
 
+if ('adapter' in key)
+{
+    const ifaces = os.networkInterfaces();
+    const adapter = ifaces[key.adapter];
+    if (adapter)
+    {
+        delete key.adapter;
+        key.address = adapter[0].address;
+    }
+    else
+    {
+        console.error('Adapter not found: '+key.adapter);
+        process.exit(ENOENT);
+    }
+}
+
+const json = JSON.stringify(key);
 const req = https.request({
     hostname: 'rua.kr',
     port: 443,
